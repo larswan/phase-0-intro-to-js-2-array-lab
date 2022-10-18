637dd5f30a4f43c76144cf6a3b3e03d9047ee566
Larson Collier <collierlarson@gmail.com>
2022-10-18 11:40:47 -0400

    commit tree b3d8a3165d03203271dbc2bbf26106a4e511e774

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+
+function appendCat(name){
+    const newCats = [...cats, name]
+    return newCats
+}
+
+function prependCat(name){
+    const preCats = [name, ...cats]
+    return preCats
+}
+function removeLastCat(){
+    const newKitty = cats.slice(0,-1)
+    return newKitty
+}
+function removeFirstCat(){
+    const newCats = cats.slice(1,cats.length)
+    return newCats
+}
